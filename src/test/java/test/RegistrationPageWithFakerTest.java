9bf1a25af23680487a50d1c5db125e076f5c11e0
nora <nora7373@mail.ru>
2023-11-29 15:59:34 +0300

    fix according comments

--- src/test/java/test/RegistrationPageWithFakerTest.java
+++ src/test/java/test/RegistrationPageWithFakerTest.java
@@ -1,84 +1,41 @@
 package test;
 
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import pages.components.RegistrationPage;
-import java.util.Locale;
 
 public class RegistrationPageWithFakerTest extends TestBase {
     RegistrationPage registrationTest = new RegistrationPage();
-    Faker faker = new Faker(new Locale("en-GB"));
 
+    TestData data = new TestData();
 
-    String firstName = faker.name().firstName();
-    String lastName = faker.name().lastName();
-    String fullName = firstName + " " + lastName;
-    String emailAddress = faker.internet().emailAddress();
-    String gender = faker.options().option("Male", "Female", "Other");
-    String userNumber = String.valueOf(faker.number().digits(10));
-    String dayOfBirth = Integer.toString(faker.number().numberBetween(1,28));
-    String mounthOfBirth = faker.options().option(
-            "January", "February", "March", "April", "May",
-            "June", "July", "August", "September",
-            "October", "November", "December");
-    String yearhOfBirth = Integer.toString(faker.number().numberBetween(2000,2020));
-
-    String birthday = dayOfBirth + " " + mounthOfBirth + "," + yearhOfBirth;
-    String subject = faker.options().option("Maths", "Commerce", "Arts");
-    String hobbies = faker.options().option( "Sports", "Reading", "Music");
-    String pictureName = faker.options().option("Little-cat.jpg", "Dog.jpg");
-
-    String streetAddress = faker.address().streetAddress();
-    String state = faker.options().option("NCR", "Uttar Pradesh", "Haryana", "Rajasthan");
-    String city =  getRandomCityByState(state);
-    String stateAndCity = state + " " + city;
-
-
-
-    String getRandomCityByState(String state) {
-        switch (state) {
-            case "NCR":
-                return faker.options().option("Delhi", "Gurgaon", "Noida");
-
-            case "Uttar Pradesh":
-                return faker.options().option("Agra", "Lucknow", "Merrut");
-
-            case "Haryana":
-                return faker.options().option("Karnal", "Panipad");
-
-            case "Rajasthan":
-                return faker.options().option("Jaipur", "Jaiselmer");
-        }
-        return "";
-    }
     @Test
     void successfulRegistrationTest() {
         RegistrationPage registrationPage = registrationTest.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(emailAddress)
-                .setGender(gender)
-                .setUserNumber(userNumber)
-                .setDateOfBirth(dayOfBirth, mounthOfBirth, yearhOfBirth)
+                .setFirstName(data.firstName)
+                .setLastName(data.lastName)
+                .setEmail(data.emailAddress)
+                .setGender(data.gender)
+                .setUserNumber(data.userNumber)
+                .setDateOfBirth(data.dayOfBirth, data.mounthOfBirth, data.yearhOfBirth)
 
-                .setSubject(subject)
-                .setHobbiesWrapper(hobbies)
-                .setUploadFromClasspath(pictureName)
-                .setCurrentAddress(streetAddress)
-                .setSelectState(state)
-                .setSelectCity(city)
+                .setSubject(data.subject)
+                .setHobbiesWrapper(data.hobbies)
+                .setUploadFromClasspath(data.pictureName)
+                .setCurrentAddress(data.streetAddress)
+                .setSelectState(data.state)
+                .setSelectCity(data.city)
                 .setSubmit();
 
-        registrationPage.checkResult("Student Name", fullName)
-                .checkResult("Student Email", emailAddress)
-                .checkResult("Gender", gender)
-                .checkResult("Mobile", userNumber)
-                .checkResult("Date of Birth", birthday)
-                .checkResult("Subjects", subject)
-                .checkResult("Hobbies", hobbies)
-                .checkResult("Picture", pictureName)
-                .checkResult("Address", streetAddress)
-                .checkResult("State and City", stateAndCity);
+        registrationPage.checkResult("Student Name", data.fullName)
+                .checkResult("Student Email", data.emailAddress)
+                .checkResult("Gender", data.gender)
+                .checkResult("Mobile", data.userNumber)
+                .checkResult("Date of Birth", data.birthday)
+                .checkResult("Subjects", data.subject)
+                .checkResult("Hobbies", data.hobbies)
+                .checkResult("Picture", data.pictureName)
+                .checkResult("Address", data.streetAddress)
+                .checkResult("State and City", data.stateAndCity);
 
     }
 
